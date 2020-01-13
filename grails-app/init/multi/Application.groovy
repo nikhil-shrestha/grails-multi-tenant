22d46876b6883266421ff6e869ddbaeb6e1f25d4
illusionist <nikhil.shrestha1995@gmail.com>
2020-01-13 13:22:46 +0545

    create default Manufacturer

--- grails-app/init/multi/Application.groovy
+++ grails-app/init/multi/Application.groovy
@@ -2,12 +2,25 @@ package multi
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import multi.tenant.Manufacturer
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import grails.gorm.transactions.Transactional
 import groovy.transform.CompileStatic
 
 @CompileStatic
-class Application extends GrailsAutoConfiguration {
+class Application extends GrailsAutoConfiguration implements ApplicationRunner {
 
     static void main(String[] args) {
         GrailsApp.run(Application, args)
+    }
+
+    @Override
+    @Transactional
+    void run(ApplicationArguments args) throws Exception {
+        Manufacturer.saveAll(
+          new Manufacturer(name: 'Audi'),
+          new Manufacturer(name: 'Ford')
+        )
     }
 }
